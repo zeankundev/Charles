53c6274320b727a54656e6ff99a90fcabde949fb
zeankun <zeanfender11@gmail.com>
2025-06-28 18:49:17 +0700

    idk what did i change

--- src/js/WindowManager.js
+++ src/js/WindowManager.js
@@ -5,3 +5,26 @@ AddListener({
         remote.getCurrentWindow().minimize();
     }
 });
+AddListener({
+    id: 'maximize',
+    event: 'click',
+    fx: () => {
+        const win = remote.getCurrentWindow();
+        if (win.isMaximized()) {
+            win.unmaximize();
+        } else {
+            win.maximize();
+        }
+    }
+});
+AddGlobalListener({
+    event: 'resize',
+    fx: () => {
+        const win = remote.getCurrentWindow();
+        if (win.isMaximized()) {
+            GetElement('maximize-icon').src = './icons/window/restore.svg';
+        } else {
+            GetElement('maximize-icon').src = './icons/window/maximize.svg';
+        }
+    }
+});
