991686a1d9149a76c80e7b9d9c3b9f8033e5c9c8
zeankun <zeanfender11@gmail.com>
2025-06-28 15:34:51 +0700

    set up electron

---
+++ entry.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        autoHideMenuBar: true,
+        titleBarStyle: 'hidden',
+        frame: false
+    });
+
+    win.loadFile('src/index.html');
+    try {
+        require('electron-reloader')
+    }catch{}
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
